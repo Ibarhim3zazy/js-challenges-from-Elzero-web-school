bfe1675b93274758a73c3ba6ec5cf77a91ca7ebd
Ibrahim 3zazy <83598736+Ibarhim3zazy@users.noreply.github.com>
2022-01-29 10:47:12 +0200

    Add files via upload

---
+++ js/challenge_4.js
@@ -0,0 +1,25 @@
+// Challeng From vid num 30 (*.*);
+
+let a = "Elzero Web School";
+
+// Challenge 1
+
+console.log("_______________Challenge 1_________________");
+
+console.log(a.slice(a.indexOf(a.charAt(2)), 7));
+
+console.log("_______________Challenge 2_________________");
+
+console.log(a.charAt(a.length - 4).toUpperCase().repeat(8));
+
+console.log("_______________Challenge 3_________________");
+
+console.log(a.split(" ", 1));
+
+console.log("_______________Challenge 4_________________");
+
+console.log(a.substr(0, 7) + a.substr(-6, 6));
+
+console.log("_______________Challenge 5_________________");
+
+console.log(a.substr(0, 1).toLowerCase() + a.substr(0, a.length - 1).toUpperCase() + a.substr(a.length - 1, a.length).toLowerCase());
