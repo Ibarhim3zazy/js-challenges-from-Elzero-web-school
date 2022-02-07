1b4a33c468da721b6dc91f7a6ab0778280125a8b
Ibrahim3zazy <Ibrahim3zazy@hotmail.com>
2022-02-07 11:47:13 +0200

    Challenge_6

---
+++ js/challenge_6.js
@@ -0,0 +1,55 @@
+// Challeng From vid num 39 (*.*);
+
+// Challenge 1
+
+console.log("_______________Challenge 1_________________");
+
+let job = "Manager";
+let salary = 0;
+
+switch (job) {
+  case "Manager":
+    salary = 8000;
+    console.log("My salary is " + salary);
+    break;
+  case "IT":
+  case "Support":
+    salary = 6000;
+    console.log("My salary is " + salary);
+    break;
+  case "Developer":
+  case "Designer":
+    salary = 7000;
+    console.log("My salary is " + salary);
+    break;
+  default:
+    salary = 4000;
+    console.log("My salary is " + salary);
+}
+
+// Challenge 1
+
+console.log("_______________Challenge 1_________________");
+
+let holidays = 6;
+let money = 0;
+
+if (holidays === 0) {
+  money = 5000;
+  console.log(`My Money is ${money}`);
+} else if (holidays === 1 || holidays === 2) {
+  money = 3000;
+  console.log(`My Money is ${money}`);
+} else if (holidays === 3) {
+  money = 3000;
+  console.log(`My Money is ${money}`);
+} else if (holidays === 4) {
+  money = 1000;
+  console.log(`My Money is ${money}`);
+} else if (holidays === 5) {
+  money = 0;
+  console.log(`My Money is ${money ?? `none.`}`);
+} else {
+  money = 0;
+  console.log(`My Money is ${money || `none.`}`);
+}
